78e12c435edcecabf10c045c73bff1c3641c5452
dhawalgupta06 <dhawalgupta06@gmail.com>
2026-08-01 17:20:32 +0530

    Implement overall and revenue performance SQL analysis

---
+++ 03_Analysis/01_Overall_Business_Performance_Analysis/SQL/01_overall_performance_metrics.sql
@@ -0,0 +1,109 @@
+
+-- Metric: Total Revenue
+-- Period: All Available Data
+-- Description: Calculates total revenue after item-level discounts.
+
+select
+    round(
+        sum(
+            pv.selling_price * od.quantity * (1 - coalesce(od.discount_percentage, 0) / 100)
+        ),
+        2
+    ) as total_revenue
+from
+    product_variants as pv
+    inner join order_details as od on pv.variant_id = od.variant_id;
+
+
+-- Metric: Gross Profit
+-- Period: All Available Data
+-- Description: Calculates gross profit by subtracting the cost of goods sold from discounted revenue.
+
+select
+    round(
+        sum(
+            (
+                pv.selling_price * od.quantity * (1 - coalesce(od.discount_percentage, 0) / 100)
+            ) - (
+                pv.cost_price * od.quantity
+            )
+        ),
+        2
+    ) as gross_profit
+from
+    product_variants as pv
+    inner join order_details as od on pv.variant_id = od.variant_id;
+    
+
+-- Metric: Gross Profit Margin
+-- Period: All Available Data
+-- Description: Calculates gross profit as a percentage of total revenue.
+
+select
+    round(
+        (
+            sum(
+                (
+                    pv.selling_price * od.quantity * (1 - coalesce(od.discount_percentage, 0) / 100)
+                ) - (
+                    pv.cost_price * od.quantity
+                )
+            ) / nullif(
+                sum(
+                    pv.selling_price * od.quantity * (1 - coalesce(od.discount_percentage, 0) / 100)
+                ),
+                0
+            )
+        ) * 100,
+        2
+    ) as gross_profit_margin
+from
+    product_variants as pv
+    inner join order_details as od on pv.variant_id = od.variant_id;
+
+
+-- Metric: Total Orders
+-- Period: All Available Data
+-- Description: Calculates the total number of orders placed.
+
+select
+    count(order_id) as total_orders
+from
+    orders;
+
+
+-- Metric: Units Sold
+-- Period: All Available Data
+-- Description: Calculates the total number of units sold.
+
+select
+    sum(quantity) as units_sold
+from
+    order_details;
+
+
+-- Metric: Average Order Value
+-- Period: All Available Data
+-- Description: Calculates the average revenue generated per order.
+
+select
+    round(
+        sum(
+            pv.selling_price * od.quantity * (1 - coalesce(od.discount_percentage, 0) / 100)
+        ) / 
+            count(distinct od.order_id)
+        ,
+        2)
+     as average_order_value
+from
+    order_details as od
+    inner join product_variants as pv on od.variant_id = pv.variant_id;
+
+-- Metric: Total Customers
+-- Period: All Available Data
+-- Description: Calculates the total number of unique customers who have placed at least one order.
+
+select
+    count(distinct customer_id) as total_customers
+from
+    orders;
---
+++ 03_Analysis/01_Overall_Business_Performance_Analysis/SQL/02_revenue_performance.sql
@@ -0,0 +1,141 @@
+-- Metric: Total Revenue
+-- Period: Past 3 years (2023-2025)
+-- Description: Calculates total revenue after item-level discounts.
+select
+    round(
+        sum(
+            pv.selling_price * od.quantity * (1 - coalesce(od.discount_percentage, 0) / 100)
+        ),
+        2
+    ) as total_revenue
+from
+    product_variants as pv
+    inner join order_details as od on pv.variant_id = od.variant_id
+    inner join orders o on o.order_id = od.order_id
+where
+    o.order_datetime >= '2023-01-01'
+    and o.order_datetime < '2026-01-01';
+
+-- Metric : Revenue by Year
+-- Period : Past 3 years (2023-2025)
+-- Description : Calculates revenue of past 3 years.
+select
+    year(o.order_datetime) as year,
+    round(
+        sum(
+            pv.selling_price * od.quantity *(1 -(coalesce(od.discount_percentage, 0) / 100))
+        ),
+        2
+    ) as revenue
+from
+    product_variants pv
+    inner join order_details od on pv.variant_id = od.variant_id
+    inner join orders o on o.order_id = od.order_id
+where
+    o.order_datetime >= '2023-01-01'
+    and o.order_datetime < '2026-01-01'
+group by
+    year(o.order_datetime)
+order by
+    year(o.order_datetime) asc;
+
+-- Metric : Revenue by Month
+-- Period : Past 3 years (2023-2025)
+-- Description : Calculates revenue of past 3 years.
+select
+    monthname(o.order_datetime) as month,
+    year(o.order_datetime) as year,
+    round(
+        sum(
+            pv.selling_price * od.quantity *(1 -(coalesce(od.discount_percentage, 0) / 100))
+        ),
+        2
+    ) as revenue
+from
+    product_variants pv
+    inner join order_details od on pv.variant_id = od.variant_id
+    inner join orders o on o.order_id = od.order_id
+where
+    o.order_datetime >= '2023-01-01'
+    and o.order_datetime < '2026-01-01'
+group by
+    monthname(o.order_datetime),
+    month(o.order_datetime),
+    year(o.order_datetime)
+order by
+    year(o.order_datetime) asc,
+    month(o.order_datetime);
+
+-- Metric : Revenue by Quarter
+-- Period : Past 3 years (2023-2025)
+-- Description : Calculates revenue of past 3 years.
+select
+    quarter(o.order_datetime) as quarter,
+    year(o.order_datetime) as year,
+    round(
+        sum(
+            pv.selling_price * od.quantity *(1 -(coalesce(od.discount_percentage, 0) / 100))
+        ),
+        2
+    ) as revenue
+from
+    product_variants pv
+    inner join order_details od on pv.variant_id = od.variant_id
+    inner join orders o on o.order_id = od.order_id
+where
+    o.order_datetime >= '2023-01-01'
+    and o.order_datetime < '2026-01-01'
+group by
+    quarter(o.order_datetime),
+    year(o.order_datetime)
+order by
+    year(o.order_datetime) asc,
+    quarter(o.order_datetime) asc;
+
+-- Metric : Revenue Growth %
+-- Period : Last 3 year (2023-2025)
+-- Description : Calculates revenue growth rate in comparison of last year revenue.
+with revenue_table as (
+    select
+        year(o.order_datetime) as year,
+        round(
+            sum(
+                pv.selling_price * od.quantity * (1 - coalesce(od.discount_percentage, 0) / 100)
+            ),
+            2
+        ) as total_revenue
+    from
+        product_variants as pv
+        inner join order_details as od on pv.variant_id = od.variant_id
+        inner join orders as o on o.order_id = od.order_id
+    where
+        o.order_datetime >= '2023-01-01'
+        and o.order_datetime < '2026-01-01'
+    group by
+        year(o.order_datetime)
+)
+select
+    year,
+    total_revenue,
+    lag(total_revenue) over (
+        order by
+            year
+    ) as previous_year_revenue,
+    round(
+        (
+            (
+                total_revenue - lag(total_revenue) over (
+                    order by
+                        year
+                )
+            ) * 100
+        ) / lag(total_revenue) over (
+            order by
+                year
+        ),
+        2
+    ) as revenue_growth_percentage
+from
+    revenue_table
+order by
+    year;
